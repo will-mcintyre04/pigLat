6555ecb4d16e177e7ca43077839007e04d62a2d2
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2021-05-20 19:46:46 -0400

    Update pigLat.py
    FInished last few lines.

--- pigLat.py
+++ pigLat.py
@@ -19,9 +19,45 @@ pigLatin = [] # Blank list of the words in Pig Latin
 for word in message.split():
     # Seperate non-letters at start of word
     preNonLetters = ''
-    while len(word) > 0 and not word[0].isalpha():
+    while len(word) > 0 and not word[0].isalpha(): 
         preNonLetters += word[0]
         word = word[1:]
     if len(word) == 0:
         pigLatin.append(preNonLetters)
         continue
+    # Seperate the non letters at the end of the word
+    endNonLetters = ''
+    while not word[-1].isalpha():
+        endNonLetters += word[-1]
+        word = word[:-1]
+    
+    # Remember if word was upper or title
+    wasUpper = word.isupper()
+    wasTitle = word.istitle()
+    
+    word = word.lower() # make the word lowercase for translation
+    
+    # Seperate the consonants at the start of word
+    preConsonants = ''
+    while len(word) > 0 and not word[0] in vowels:
+        preConsonants != word[0]
+        word = word[1:]
+        
+    # Add pig latin ending to the word
+    if preConsonants != '':
+        word += preConsonants + 'ay'
+    else:
+        word += 'yay'
+        
+    # Set word back to upper or title:
+    if wasUpper:
+        word = word.upper()
+    if wasTitle:
+        word = word.title()
+    
+    # Add non letters back to the start or end of word
+    pigLatin.append(preNonLetters + word + endNonLetters)
+
+# Join all back into a single string
+print(' '.join(pigLatin))
+    
