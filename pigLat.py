a9ece29508e7b424ce32df9a1a5ac80e2b65d284
will-mcintyre04 <william.d.j.mcintyre@gmail.com>
2021-05-20 18:56:30 -0400

    Update pigLat.py
    Blank list, and get rid of unnecesary non-letters

--- pigLat.py
+++ pigLat.py
@@ -7,3 +7,21 @@ Created on Thu May 20 18:40:13 2021
 """
 
 # English to Pig Latin
+# Will McIntyre
+# May 20th, 2021
+
+print('Enter the English message to translate into Pig Latin: ')
+message = input()
+
+vowels = ('a', 'e', 'i', 'o', 'u', 'y')
+
+pigLatin = [] # Blank list of the words in Pig Latin
+for word in message.split():
+    # Seperate non-letters at start of word
+    preNonLetters = ''
+    while len(word) > 0 and not word[0].isalpha():
+        preNonLetters += word[0]
+        word = word[1:]
+    if len(word) == 0:
+        pigLatin.append(preNonLetters)
+        continue
